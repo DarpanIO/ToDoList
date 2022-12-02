0b2c4cb016d3b29fdb4b856f8cbabd1a31ca65fc
darpan9460@gmail.com <darpan9460@gmail.com>
2022-12-02 20:09:03 +0530

    Updates

---
+++ precache-manifest.d0690c3cd0e2820ca796788195b7f609.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4171f4d025da4d7125e2ec3ddd9d35",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "9302e28affcbd310209f",
+    "url": "/ToDoList/static/js/2.17e1ab02.chunk.js"
+  },
+  {
+    "revision": "3a576fd1cb481cff3337",
+    "url": "/ToDoList/static/js/main.b324f3ba.chunk.js"
+  },
+  {
+    "revision": "0f27c0577bc8a492b5c5",
+    "url": "/ToDoList/static/js/runtime-main.b8ed885d.js"
+  }
+]);
